7023aa58be7141751ca6df164e98b22122d84409
airways <isaac.raway@gmail.com>
2011-06-07 22:26:45 +0000

    Update proform version, update build script to automatically tag releases
    git-svn-id: https://svn.metasushi.net/ee/trunk/addons/proform@189 59723c01-6204-4710-ac68-99f48fd4fa17

--- upd.proform.php
+++ upd.proform.php
@@ -31,8 +31,8 @@
  **/
 
 class Proform_upd {
-    // @version 1.7
-    var $version = "1.7";
+    // @version 1.8
+    var $version = "1.8";
     
     function Proform_upd() {
         $this->EE = &get_instance();
