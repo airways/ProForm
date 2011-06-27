c5ce43105053b392e5afce4e5f59575cce6974cc
airways <isaac.raway@gmail.com>
2011-06-27 18:13:46 +0000

    Update proform version
    git-svn-id: https://svn.metasushi.net/ee/trunk/addons/proform@270 59723c01-6204-4710-ac68-99f48fd4fa17

--- upd.proform.php
+++ upd.proform.php
@@ -31,8 +31,8 @@
  **/
 
 class Proform_upd {
-    // @version 1.9.4
-    var $version = "1.9.4";
+    // @version 1.9.5
+    var $version = "1.9.5";
     
     function Proform_upd() {
         $this->EE = &get_instance();
