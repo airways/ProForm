dfc9a7a92525d83c3d5ab11334edd6e160be0b35
Isaac Raway <isaac.raway@gmail.com>
2011-10-30 15:57:21 -0500

    Remove old tests directory

--- __tests/forms_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(APPPATH.'/libraries/simpletest/autorun.php');
-
-class CanAddUp extends UnitTestCase {
-    function testOneAndOneMakesTwo() {
-        $this->assertEqual(1 + 1, 2);
-    }
-}
