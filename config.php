6796dcbc2bcbed95eb07a844a736195a7ab529a3
airways <isaac.raway@gmail.com>
2011-11-03 13:59:10 -0500

    Test

--- config.php
+++ config.php
@@ -7,3 +7,5 @@ define('PROFORM_CLASS', 'Proform'); // must match module class name
 define('PROFORM_DESCRIPTION', 'ProForm is an advanced form management module for ExpressionEngine 2.0, designed to make creation and management of forms easier for developers and end users.');
 define('PROFORM_DOCSURL', 'http://metasushi.com/docs/proform');
 define('PROFORM_DEBUG', TRUE);
+
+// Test
