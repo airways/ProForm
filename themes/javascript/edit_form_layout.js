db2454b1a2a9220987a97ced527d74e88511082c
Isaac Raway <isaac.raway@gmail.com>
2012-02-28 22:31:48 -0600

    Updates

--- themes/javascript/edit_form_layout.js
+++ themes/javascript/edit_form_layout.js
@@ -69,7 +69,101 @@ var proform_edit_form_layout = {
                 }
             });
         }
+        
+        
+        // var label_edited = function() { 
+        // $('#field-label').keydown(label_edited).change(label_edited);
+        
+    
+        $('#gridrow_validation tr:odd').addClass('even');
 
+        var $active_field = 0;
+        var loading_meta = false;
+
+        var label_updated = function() {
+            if($active_field && !loading_meta) {
+                if($('#field-label').val().trim() != '')
+                {
+                    $active_field.find('label').text($('#field-label').val());
+                } else {
+                    $active_field.find('label').text($active_field.find('.fieldOriginalLabel').val());
+                } 
+            }
+        }
+        $('#field-label').keydown(label_updated).keyup(label_updated).change(label_updated);
+
+        var default_updated = function() {
+            if($active_field && !loading_meta) {
+                if($('#field-preset-value').val().trim() != '')
+                {
+                    $active_field.find('.placeHolder').val($('#field-preset-value').val());
+                } else {
+                    $active_field.find('.placeHolder').val('');
+                } 
+            }
+        }
+        $('#field-preset-value').keydown(default_updated).keyup(default_updated).change(default_updated);
+        
+        function save_meta()
+        {
+            if($active_field)
+            {
+                $active_field.find('.fieldRequired').val($('#field-required').is(':checked') ? 'y' : 'n');
+                $active_field.find('.fieldLabel').val($('#field-label').val());
+                $active_field.find('.fieldPresetValue').val($('#field-preset-value').val());
+                $active_field.find('.fieldPresetForced').val($('#field-preset-forced').is(':checked') ? 'y' : 'n');
+                $active_field.find('.fieldHtmlId').val($('#field-html-id').val());
+                $active_field.find('.fieldHtmlClass').val($('#field-html-class').val());
+                $active_field.find('.fieldExtra1').val($('#field-extra1').val());
+                $active_field.find('.fieldExtra2').val($('#field-extra2').val());
+                label_updated();
+                default_updated();
+            }
+        }
+
+        function load_meta()
+        {
+            if($active_field)
+            {
+                loading_meta = true;
+                
+                $('.meta-sidebar').show();
+                if($active_field.find('.isHeading').val() == '1') {
+                    $('.meta-sidebar').hide();
+                }
+            
+                $('#field-required').attr('checked', $active_field.find('.fieldRequired').val() == 'y');
+                $('#field-label').val($active_field.find('.fieldLabel').val());
+                $('#field-preset-value').val($active_field.find('.fieldPresetValue').val());
+                $('#field-preset-forced').attr('checked', $active_field.find('.fieldPresetForced').val() == 'y');
+                $('#field-html-id').val($active_field.find('.fieldHtmlId').val());
+                $('#field-html-class').val($active_field.find('.fieldHtmlClass').val());
+                $('#field-extra1').val($active_field.find('.fieldExtra1').val());
+                $('#field-extra2').val($active_field.find('.fieldExtra2').val());
+                
+                loading_meta = false;
+            }
+        }
+        
+        // disable all property inspector inputs until a field is selected
+        $('.field-modifications input[type=text],.field-modifications input[type=checkbox]').attr('disabled', 'disabled');
+    
+        $('.form-setup li').click(function() {
+            save_meta();
+            $('.form-setup li').removeClass('active');
+        
+            $active_field = $(this);
+            $(this).addClass('active');
+            load_meta();
+        
+            $('#edit-field-name').text($active_field.find('.fieldLabel'));
+            $('.field-modifications input').removeAttr('disabled');
+        
+        });
+    
+        $('#main_form').submit(function() {
+            save_meta();
+        });
    }
 };
 
