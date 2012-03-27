8d89f4ce238d3f8e5a1d11ff2635000af8d63adb
Isaac Raway <isaac.raway@gmail.com>
2012-03-26 22:31:44 -0500

    Disable automatic Field Name generation from Field Label, if editing an existing field.

--- themes/javascript/edit_field.js
+++ themes/javascript/edit_field.js
@@ -6,7 +6,12 @@ var proform_edit_field = {
             proform_edit_field.update_settings_fields();
         });
        
-        var update_field_label = function() { $('input[name=field_name]').val(proform_mod.make_name($(this).val())); }
+        var update_field_label = function() { 
+            if(!$('input[name=field_id]').val())
+            {
+                $('input[name=field_name]').val(proform_mod.make_name($(this).val()));
+            }
+        }
         $('input[name=field_label]').keydown(update_field_label).keyup(update_field_label).change(update_field_label);
 
         $('form').submit(function() {
