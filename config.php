166fc218034eb7aaab19081c57b83cb76d43618e
Isaac Raway <isaac@mm.st>
2012-09-24 22:35:05 -0500

    Update version to 1.24, updated prolib dependency to 0.55

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-// @version 1.23
-// @prolib 0.54
+// @version 1.24
+// @prolib 0.55
 
-define('PROFORM_VERSION', '1.23');
+define('PROFORM_VERSION', '1.24');
 define('PROFORM_NAME', 'ProForm');
 define('PROFORM_CLASS', 'Proform'); // must match module class name
 define('PROFORM_DESCRIPTION', 'ProForm is an advanced form management module for ExpressionEngine 2.0, designed to make creation and management of forms easier for developers and end users.');
