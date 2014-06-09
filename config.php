89b692741cb2d7627e0ba51ac56a1399dc8c8e9b
Isaac Raway <isaac@mm.st>
2014-06-08 19:03:24 -0500

    Fixed version check for cp_url() and ee() functions in config.php

--- config.php
+++ config.php
@@ -22,3 +22,26 @@ if (version_compare(APP_VER, '2.6', '<') && !function_exists('ee'))
         return $EE;
     }
 }
+
+// EE 2.8 cp_url function is now used to generate URLs - need to provide it if
+// we are on a version prior to EE 2.8
+if (version_compare(APP_VER, '2.8', '<') && !function_exists('ee'))
+{
+    function cp_url($path, $qs = '')
+    {
+    	$path = trim($path, '/');
+    	$path = preg_replace('#^cp(/|$)#', '', $path);
+        
+        $segments = explode('/', $path);
+        $result = BASE.AMP.'C='.$segments[0].AMP.'M='.$segments[1];
+        
+    	if (is_array($qs))
+    	{
+    		$qs = AMP.http_build_query($qs, AMP);
+    	}
+    	
+    	$result .= $qs;
+    
+    	return $result;
+    }
+}
