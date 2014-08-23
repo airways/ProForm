406a4c4105b4627c8e908d342cfb7e40419e9a6f
Isaac Raway <isaac@mm.st>
2014-08-22 20:55:48 -0500

    - Bump tag to 1.52

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-// @version 1.50
+// @version 1.52
 // @prolib 0.75
 
-define('PROFORM_VERSION', '1.50');
+define('PROFORM_VERSION', '1.52');
 define('PROFORM_NAME', 'ProForm');
 define('PROFORM_CLASS', 'Proform'); // must match module class name
 define('PROFORM_DESCRIPTION', 'ProForm is an advanced form management module for ExpressionEngine 2.0, designed to make creation and management of forms easier for developers and end users.');
