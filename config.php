e6f538d1c77282d36dce5328091461232a34c4ad
Isaac Raway <isaac@mm.st>
2014-09-26 22:14:10 -0500

    Bump version to 1.61

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-// @version 1.60
+// @version 1.61
 // @prolib 0.77
 
-define('PROFORM_VERSION', '1.60');
+define('PROFORM_VERSION', '1.61');
 define('PROFORM_NAME', 'ProForm');
 define('PROFORM_CLASS', 'Proform'); // must match module class name
 define('PROFORM_DESCRIPTION', 'ProForm is an advanced form management module for ExpressionEngine 2.0, designed to make creation and management of forms easier for developers and end users.');
