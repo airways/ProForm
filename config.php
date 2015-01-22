fe16fcac8e907f5de63bce8fa20e39a9351eceae
Isaac Raway <isaac@mm.st>
2015-01-22 15:01:12 -0600

    Bump prolib dependency to 0.78 to get improved parsing speed for form tag

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-// @version 1.64
-// @prolib 0.77
+// @version 1.65
+// @prolib 0.78
 
-define('PROFORM_VERSION', '1.64');
+define('PROFORM_VERSION', '1.65');
 define('PROFORM_NAME', 'ProForm');
 define('PROFORM_CLASS', 'Proform'); // must match module class name
 define('PROFORM_DESCRIPTION', 'ProForm is an advanced form management module for ExpressionEngine 2.0, designed to make creation and management of forms easier for developers and end users.');
